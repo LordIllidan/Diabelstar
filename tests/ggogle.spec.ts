2ab5fed77db775d1ea90d994b36e903314d454be
Krzysztof Adamiak <ilidan.shark@gmail.com>
2025-02-24 19:59:42 +0100

    Update ggogle.spec.ts

--- tests/ggogle.spec.ts
+++ tests/ggogle.spec.ts
@@ -2,9 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.google.pl/?gws_rd=ssl');
-  await page.getByRole('button', { name: 'Zaakceptuj wszystko' }).click();
-  await page.getByRole('button', { name: 'Szczęśliwy traf' }).click();
-  await page.locator('.hero-tag-carousel__collage').first().click();
-  await page.getByRole('button', { name: 'OK, got it' }).click();
-  await page.getByRole('button', { name: 'Surprise me' }).click();
+  
 });
